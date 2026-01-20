9497a48a6c86e3a81bced45c5e19fed21666279b
Saksham gupta <163002102+Adibotkaiaia@users.noreply.github.com>
2026-01-20 17:40:43 +0530

    Update __init__.py

--- Mickey/modules/__init__.py
+++ Mickey/modules/__init__.py
@@ -1,26 +1,12 @@
 import glob
 from os.path import basename, dirname, isfile
 
-# ----------------------------
-# Load all modules automatically
-# ----------------------------
 def __list_all_modules():
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
-
-    modules = [
+    files = glob.glob(dirname(__file__) + "/*.py")
+    return [
         basename(f)[:-3]
-        for f in mod_paths
-        if isfile(f)
-        and f.endswith(".py")
-        and not f.endswith("__init__.py")
+        for f in files
+        if isfile(f) and not f.endswith("__init__.py")
     ]
-
-    # ai_reply ko last load karna (safe)
-    if "ai_reply" in modules:
-        modules.remove("ai_reply")
-        modules.append("ai_reply")
-
-    return modules
-
 
 ALL_MODULES = __list_all_modules()
