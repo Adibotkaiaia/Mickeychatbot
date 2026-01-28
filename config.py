977b5ae8ce833327060c1460077e4ae3aeef9974
Saksham gupta <163002102+Adibotkaiaia@users.noreply.github.com>
2026-01-28 08:13:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,16 +10,19 @@ API_ID = int(getenv("API_ID", 21446101))
 API_HASH = getenv("API_HASH", "0a2594b17b59f8928aa240825d898df1")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
+
+# AI KEYS
 OPENAI_API_KEY = getenv("OPENAI_API_KEY", None)
+GEMINI_API_KEY = getenv("GEMINI_API_KEY", None)   # 👈 ADD THIS
 
 # ==========================
 # === OWNER & ADMINS ===
 # ==========================
-OWNER_ID = int(getenv("OWNER_ID", None ))  # <-- apna Telegram ID
+OWNER_ID = int(getenv("OWNER_ID", None))   # <-- apna Telegram ID
 OWNER_USERNAME = getenv("OWNER_USERNAME", "I_ADI_I")
 
 # List of additional admins (Telegram IDs)
-ADMIN_IDS = [8400280060]  # <-- apne team/admin IDs yahan dal sakte ho
+ADMIN_IDS = [8400280060]
 
 # ==========================
 # === OPTIONAL LINKS ===
@@ -38,8 +41,6 @@ REVIVE_SECONDS = 3600      # Time before a dead user can revive (1 hour)
 # ==========================
 IMG = [
     "https://te.legra.ph/file/5bf629d10afd4af953585.jpg",
-    #"https://te.legra.ph/file/7a321b99fe99d9d8b5117.jpg",
-    #"https://te.legra.ph/file/c482a7e55b459ffe07502.jpg",
 ]
 
 # ==========================
